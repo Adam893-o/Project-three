e30e902d0bc577048a9bbf46e2c669830189f8cd
Adam893-o <asharifeh@hawk.iit.edu>
2024-10-23 19:07:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,13 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: 0, lng: -180 },
-    mapTypeId: "terrain",
-  });
-  const flightPlanCoordinates = [
-    { lat: 37.772, lng: -122.214 },
-    { lat: 21.291, lng: -157.821 },
-    { lat: -18.142, lng: 178.431 },
-    { lat: -27.467, lng: 153.027 },
-  ];
-  const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+function init(){
+  var button = document.getElementById('entrybutton');
 
-  flightPath.setMap(map);
-}
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Daniel Krieglstein: " + textbox.value);
+  }
 
-function initMap() {
-  const center = new google.maps.LatLng(-33.712451, 150.311823);
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 9,
-    center: center,
-  });
-  const svgMarker = {
-    path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
-    fillColor: "blue",
-    fillOpacity: 0.6,
-    strokeWeight: 0,
-    rotation: 0,
-    scale: 2,
-    anchor: new google.maps.Point(0, 20),
-  };
-
-  new google.maps.Marker({
-    position: map.getCenter(),
-    icon: svgMarker,
-    map: map,
-  });
-}
-
-window.initMap = initMap;
+  button.addEventListener('click', showMeText);
+  }
+  
+window.addEventListener('load', init);

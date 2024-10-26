8bcf697f4d705a08a2ca42126eeab75d6a81b1b2
Adam893-o <asharifeh@hawk.iit.edu>
2024-10-26 18:55:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,11 @@
-async function initMap() {
-  // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-  const myLatlng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatlng,
-    mapId: "DEMO_MAP_ID",
-  });
-  const marker = new google.maps.marker.AdvancedMarkerElement({
-    position: myLatlng,
-    map,
-    title: "Click to zoom",
-  });
+let map;
 
-  map.addListener("center_changed", () => {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(() => {
-      map.panTo(marker.position);
-    }, 3000);
-  });
-  marker.addListener("click", () => {
-    map.setZoom(8);
-    map.setCenter(marker.position);
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
 

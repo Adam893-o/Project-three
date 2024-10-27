a3bc75d7c9dc033183281c66e3180851027afead
Adam893-o <asharifeh@hawk.iit.edu>
2024-10-26 20:07:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-let map;
+let marker;
 
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: { lat: 59.325, lng: 18.07 },
   });
+
+  marker = new google.maps.Marker({
+    map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 59.327, lng: 18.067 },
+  });
+  marker.addListener("click", toggleBounce);
 }
 
-initMap();
+var myMap = new google.maps.Map(el, mapOptions);
+
+var marker = new google.mapsMarker({
+  map: myMap,
+  animation: google.maps.Animation.BOUNCE,
+  icon: 'BugsBunny-icon.jpeg'
+
+var contentString =<h1>About Jupiter</h1> <p>Jupiter has helped us understood the origins of our universe and how it was formed if it werent for this massive gas giant.</p>
+
+var infowindow = new google.maps.InfoWindows({
+  content: contentString
+)};
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+window.initMap = initMap;

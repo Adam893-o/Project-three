5dc5074d21d70cb774fe7ae62bcafb5fa4ade0a1
Adam893-o <asharifeh@hawk.iit.edu>
2024-10-30 21:22:23 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,62 +1,30 @@
-let marker;
-
-function initMap() {
+async function initMap() {
+  // Request needed libraries.
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const myLatlng = { lat: -25.363, lng: 131.044 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: { lat: 59.325, lng: 18.07 },
+    zoom: 4,
+    center: myLatlng,
+    mapId: "MY MAP",
   });
-
-  marker = new google.maps.Marker({
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+    position: myLatlng,
     map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: { lat: 59.327, lng: 18.067 },
+    title: "Click to zoom",
   });
-  marker.addListener("click", toggleBounce);
+
+  map.addListener("center_changed", () => {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(() => {
+      map.panTo(marker.position);
+    }, 3000);
+  });
+  marker.addListener("click", () => {
+    map.setZoom(8);
+    map.setCenter(marker.position);
+  });
 }
 
-new google.maps.Circle({
-  map,
-  radius: 500, 
-  fillColor: '#FF0000',
-  center: marker.getPosition(),
-});
-
-new google.maps.Polygon({
-  map,
-  paths: [
-    { lat: 59.327, lng: 18.067 },
-    { lat: 59.324, lng: 18.064 },
-    { lat: 59.321, lng: 18.068 },
-  ],
-  strokeColor: '#FF0000',
-  fillColor: '#FF0000',
-});
-
-var myMap = new google.maps.Map(el, mapOptions);
-
-var marker = new google.mapsMarker({
-  map: myMap,
-  animation: google.maps.Animation.BOUNCE,
-  icon: 'BugsBunny-icon.jpeg'
-
-const contentString =<h1>About Jupiter</h1> <p>Jupiter has helped us understood the origins of our universe and how it was formed if it werent for this massive gas giant.</p>
-
-const infowindow = new google.maps.InfoWindows({
-  content: contentString
-});
-
-marker.addListener("click", () => {
-  infowindow.open(map, marker);
- });
-
-
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
-
-window.initMap = initMap;
+initMap();

e9c7d03eaa237652a03a6bec649697b4fe29b348
Adam893-o <asharifeh@hawk.iit.edu>
2024-11-12 17:10:20 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ function initMap() {
     mapTypeId: "terrain",
   });
 
-  const squareCoords = [
+  const triangleCoords = [
     { lat: 25.774, lng: -80.19 },
     { lat: 18.466, lng: -66.118 },
     { lat: 32.321, lng: -64.757 },
     { lat: 25.774, lng: -80.19 },
   ];
 
-  const bermudaSquare = new google.maps.Polygon({
+  const bermudaTriangle = new google.maps.Polygon({
     paths: triangleCoords,
     strokeColor: "#FF0000",
     strokeOpacity: 0.8,
